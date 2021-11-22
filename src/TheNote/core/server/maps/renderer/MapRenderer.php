6300c014efa2da4a4d6861b450820ad6f4fc8097
TheNote <38653750+note3crafter@users.noreply.github.com>
2021-11-22 22:38:41 +0100

    CoreV6 ALPHA v.6.0.5 Update
    -v.6.0.5 ALPHA 
    Blöcke und Items wurden weiter bearbeitet
    Statstext funktioniert wieder
    WorldCommand wieder vollständig nutzbar
    EconomyShop & Sell Bugfix
    AFKCommand Fix
    HubCommand fix
    Emotes Fix
    
    Danke an galaxytweenty für fixes in den Blöcken :D

---
+++ src/TheNote/core/server/maps/renderer/MapRenderer.php
@@ -0,0 +1,28 @@
+<?php
+
+//   ╔═════╗╔═╗ ╔═╗╔═════╗╔═╗    ╔═╗╔═════╗╔═════╗╔═════╗
+//   ╚═╗ ╔═╝║ ║ ║ ║║ ╔═══╝║ ╚═╗  ║ ║║ ╔═╗ ║╚═╗ ╔═╝║ ╔═══╝
+//     ║ ║  ║ ╚═╝ ║║ ╚══╗ ║   ╚══╣ ║║ ║ ║ ║  ║ ║  ║ ╚══╗
+//     ║ ║  ║ ╔═╗ ║║ ╔══╝ ║ ╠══╗   ║║ ║ ║ ║  ║ ║  ║ ╔══╝
+//     ║ ║  ║ ║ ║ ║║ ╚═══╗║ ║  ╚═╗ ║║ ╚═╝ ║  ║ ║  ║ ╚═══╗
+//     ╚═╝  ╚═╝ ╚═╝╚═════╝╚═╝    ╚═╝╚═════╝  ╚═╝  ╚═════╝
+//   Copyright by TheNote! Not for Resale! Not for others
+//
+
+namespace TheNote\core\server\maps\renderer;
+
+use TheNote\core\server\maps\MapData;
+use pocketmine\Player;
+
+abstract class MapRenderer{
+
+	public function initialize(MapData $mapData) : void{
+
+	}
+
+	public abstract function render(MapData $mapData, Player $player) : void;
+
+	public function onMapCreated(Player $player, MapData $mapData) : void{
+
+	}
+}
