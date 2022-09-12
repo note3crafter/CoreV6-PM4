32c6de38898ecb6d902f2af53c5dcd214d9aff04
TheNote <38653750+note3crafter@users.noreply.github.com>
2022-09-12 21:17:50 +0200

    CoreV6 ALPHA v6.0.11 Update
    Part5

---
+++ src/TheNote/core/utils/ArrayUtils.php
@@ -0,0 +1,27 @@
+<?php
+
+//   ╔═════╗╔═╗ ╔═╗╔═════╗╔═╗    ╔═╗╔═════╗╔═════╗╔═════╗
+//   ╚═╗ ╔═╝║ ║ ║ ║║ ╔═══╝║ ╚═╗  ║ ║║ ╔═╗ ║╚═╗ ╔═╝║ ╔═══╝
+//     ║ ║  ║ ╚═╝ ║║ ╚══╗ ║   ╚══╣ ║║ ║ ║ ║  ║ ║  ║ ╚══╗
+//     ║ ║  ║ ╔═╗ ║║ ╔══╝ ║ ╠══╗   ║║ ║ ║ ║  ║ ║  ║ ╔══╝
+//     ║ ║  ║ ║ ║ ║║ ╚═══╗║ ║  ╚═╗ ║║ ╚═╝ ║  ║ ║  ║ ╚═══╗
+//     ╚═╝  ╚═╝ ╚═╝╚═════╝╚═╝    ╚═╝╚═════╝  ╚═╝  ╚═════╝
+//   Copyright by TheNote! Not for Resale! Not for others
+//                        2017-2020
+
+namespace TheNote\core\utils;
+
+use Closure;
+
+final class ArrayUtils {
+
+    public static function firstOrDefault(array $array, Closure $condition, $fallback = null){
+        foreach($array as $index => $element){
+            if($condition($index, $element)){
+                return $element;
+            }
+        }
+        
+        return $fallback;
+    }
+}
