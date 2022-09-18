a18e2e8783bbbb9499d4e0cd384b874d3fc2aa46
TheNote <38653750+note3crafter@users.noreply.github.com>
2022-09-18 13:51:18 +0200

    OOOOOOPPPPPSSSS xd

---
+++ src/TheNote/core/tile/Tiles.php
@@ -0,0 +1,25 @@
+<?php
+
+//   ╔═════╗╔═╗ ╔═╗╔═════╗╔═╗    ╔═╗╔═════╗╔═════╗╔═════╗
+//   ╚═╗ ╔═╝║ ║ ║ ║║ ╔═══╝║ ╚═╗  ║ ║║ ╔═╗ ║╚═╗ ╔═╝║ ╔═══╝
+//     ║ ║  ║ ╚═╝ ║║ ╚══╗ ║   ╚══╣ ║║ ║ ║ ║  ║ ║  ║ ╚══╗
+//     ║ ║  ║ ╔═╗ ║║ ╔══╝ ║ ╠══╗   ║║ ║ ║ ║  ║ ║  ║ ╔══╝
+//     ║ ║  ║ ║ ║ ║║ ╚═══╗║ ║  ╚═╗ ║║ ╚═╝ ║  ║ ║  ║ ╚═══╗
+//     ╚═╝  ╚═╝ ╚═╝╚═════╝╚═╝    ╚═╝╚═════╝  ╚═╝  ╚═════╝
+//   Copyright by TheNote! Not for Resale! Not for others
+//                        2017-2020
+
+namespace TheNote\core\tile;
+
+use pocketmine\block\tile\TileFactory;
+
+class Tiles
+{
+    public static function init() :void
+    {
+        $tf = TileFactory::getInstance();
+        $tf->register(Lodestone::class, ["Lodestone", "minecraft:lodestone"]);
+
+    }
+}
+
